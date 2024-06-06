82099df22d4b4a01c496fe09d66c40296095b7e4
dajeong <uwhami@gmail.com>
2024-06-06 23:32:59 +0900

    products Add Component

---
+++ src/components/products/AddComponent.js
@@ -0,0 +1,106 @@
+import React, { useRef, useState } from "react";
+import { postAdd } from "../../api/productsApi";
+
+const initState = {
+  pname: "",
+  pdesc: "",
+  price: 0,
+  files: [],
+};
+
+// new FormData() -> POST, PUT
+
+function AddComponent(props) {
+  const [product, setProduct] = useState({ ...initState });
+
+  const uploadRef = useRef();
+
+  // multipart/form-data FormData()
+
+  const handleChangeProduct = (e) => {
+    product[e.target.name] = e.target.value;
+
+    setProduct({ ...product });
+  };
+
+  const handleClickAdd = (e) => {
+    const formData = new FormData();
+    const files = uploadRef.current.files;
+
+    for (let i = 0; i < files.length; i++) {
+      formData.append("files", files[i]);
+    }
+    formData.append("pname", product.pname);
+    formData.append("pdesc", product.pdesc);
+    formData.append("price", product.price);
+
+    console.log(formData);
+
+    postAdd(formData);
+  };
+
+  return (
+    <div className="border-2 border-sky-200 mt-10 m-2 p-4">
+      <div className="flex justify-center">
+        <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+          <div className="w-1/5 p-6 text-right font-bold">Name</div>
+          <input
+            className="w-4/5 p-6 rounded-r border border-solid border-neutral-500 shadow-md"
+            name="pname"
+            type={`text`}
+            value={product.pname}
+            onChange={handleChangeProduct}
+          ></input>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+          <div className="w-1/5 p-6 text-right font-bold">Description</div>
+          <input
+            className="w-4/5 p-6 rounded-r border border-solid border-neutral-500 shadow-md"
+            name="pdesc"
+            type={`text`}
+            value={product.pdesc}
+            onChange={handleChangeProduct}
+          ></input>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+          <div className="w-1/5 p-6 text-right font-bold">Price</div>
+          <input
+            className="w-4/5 p-6 rounded-r border border-solid border-neutral-500 shadow-md"
+            name="price"
+            type={`number`}
+            value={product.price}
+            onChange={handleChangeProduct}
+          ></input>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+          <div className="w-1/5 p-6 text-right font-bold">Files</div>
+          <input
+            ref={uploadRef}
+            className="w-4/5 p-6 rounded-r border border-solid border-neutral-500 shadow-md"
+            type={"file"}
+            multiple={true}
+          ></input>
+        </div>
+      </div>
+      <div className="flex justify-end">
+        <div className="relative mb-4 flex p-4 flex-wrap items-stretch">
+          <button
+            type="button"
+            className="rounded p-4 w-36 bg-blue-500 text-xl text-white"
+            onClick={handleClickAdd}
+          >
+            ADD
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddComponent;
---
+++ src/pages/products/AddPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AddComponent from "../../components/products/AddComponent";
+
+function AddPage(props) {
+  return (
+    <div className="p-2 w-full bg-white">
+      <div className="text-3xl font-extrabold">Products Add Page</div>
+
+      <AddComponent />
+    </div>
+  );
+}
+
+export default AddPage;
