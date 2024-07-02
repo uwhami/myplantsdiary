c1774d43120bb01be4a268c971561ca7d4c1124b
dajeong <uwhami@gmail.com>
2024-07-02 22:58:39 +0900

    logout remove cookie & accessToken axios

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -1,11 +1,11 @@
-import axios from "axios";
 import { API_SERVER_HOST } from "./todoApi";
+import jwtAxios from "../util/jwtUtil";
 
 const host = `${API_SERVER_HOST}/api/products`;
 
 export const postAdd = async (product) => {
   const header = { headers: { "Content-Type": "multipart/form-data" } };
-  const res = await axios.post(`${host}/`, product, header);
+  const res = await jwtAxios.post(`${host}/`, product, header);
 
   return res.data;
 };
@@ -16,22 +16,22 @@ export const getList = async (pageParam) => {
   //   params: { page: page, size: size },
   // });
 
-  const res = await axios.get(`${host}/list`, { params: { ...pageParam } });
+  const res = await jwtAxios.get(`${host}/list`, { params: { ...pageParam } });
   return res.data;
 };
 
 export const getOne = async (pno) => {
-  const res = await axios.get(`${host}/${pno}`);
+  const res = await jwtAxios.get(`${host}/${pno}`);
   return res.data;
 };
 
 export const deleteOne = async (pno) => {
-  const res = await axios.delete(`${host}/${pno}`);
+  const res = await jwtAxios.delete(`${host}/${pno}`);
   return res.data;
 };
 
 export const putOne = async (pno, product) => {
   const header = { headers: { "Content-Type": "multipart/form-data" } };
-  const res = await axios.put(`${host}/${pno}`, product, header);
+  const res = await jwtAxios.put(`${host}/${pno}`, product, header);
   return res.data;
 };
