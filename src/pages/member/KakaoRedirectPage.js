9785e9099c9657fb755a79723427c735e6d347b4
dajeong <uwhami@gmail.com>
2024-07-03 23:30:34 +0900

    get Kakao AccessToken

--- src/pages/member/KakaoRedirectPage.js
+++ src/pages/member/KakaoRedirectPage.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
+import { getAccessToken } from "../../api/kakaoApi";
 
 function KakaoRedirectPage(props) {
   const [searchParams] = useSearchParams();
 
   const authCode = searchParams.get("code");
+
+  useEffect(() => {
+    getAccessToken(authCode).then((data) => {
+      console.log("data", data);
+    });
+  }, [authCode]);
 
   return (
     <div>
